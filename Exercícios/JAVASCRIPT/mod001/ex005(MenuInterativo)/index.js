07936caeeec3eb9cc889d8992fcf64341516d8c0
Clayton Schaurich <44507529+claytonschaurich@users.noreply.github.com>
2024-01-03 15:15:43 -0300

    Delete Exercícios/JAVASCRIPT directory
    Decidi armazenar os arquivos em um repositório separado.

--- Exercícios/JAVASCRIPT/mod001/ex005(MenuInterativo)/index.js
+++
@@ -1,8 +0,0 @@
-let opcao = ""
-
-do {
-    opcao = prompt("Escolha uma opçao:\n Opção 1:(1) \n Opção 2:(2)\n Opção 3:(3)\n Opção 4:(4)\n Encerrar:(5)");
-    alert("Você escolheu a opção " + opcao);
-} while (opcao != "5") {
-    alert("O programa foi encerrado")
-}  
